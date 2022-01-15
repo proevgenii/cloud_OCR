a3fbd08419cd5f0d86cfd2def750748f3f0ff6e0
Evgenii Prosvirnin <proevgenii19@gmail.com>
2022-01-16 00:47:30 +0300

    add Dockerfile

--- app/app.py
+++ app/app.py
@@ -5,10 +5,10 @@ import os
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = r'app\static'
+app.config['UPLOAD_FOLDER'] = r'static/'
 
 
 @app.route('/')
@@ -20,8 +20,8 @@ def index():
 def submitImage():
     image = request.files['ocrImage']
     filename = secure_filename(image.filename)
-    image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    img = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    image.save(app.config['UPLOAD_FOLDER']+filename)
+    img = Image.open(app.config['UPLOAD_FOLDER']+filename)
     text = pytesseract.image_to_string(img)
     f = open(os.path.join(app.config['UPLOAD_FOLDER'], filename) + '.txt', 'w')
     f.write(text)
